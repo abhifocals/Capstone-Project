f8a3bfe7412c04e8427cf4231f6a54080d0ca915
Abhi Handa <ahanda@ebay.com>
2019-12-20 12:43:23 -0800

    Making a network call.

--- app/src/main/java/com/focals/myreddit/MainActivity.java
+++ app/src/main/java/com/focals/myreddit/MainActivity.java
@@ -2,7 +2,13 @@ package com.focals.myreddit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new RedditAsyncTask().execute();
+
     }
+
+
+
+
+
+    class RedditAsyncTask extends AsyncTask<Void, Void, String> {
+        HttpURLConnection httpURLConnection = null;
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://www.reddit.com/r/php/search.json?q=oop");
+
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                Scanner scanner = new Scanner(httpURLConnection.getInputStream());
+
+                if (scanner.hasNext()) {
+                    return scanner.next();
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            finally {
+                httpURLConnection.disconnect();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
 }
