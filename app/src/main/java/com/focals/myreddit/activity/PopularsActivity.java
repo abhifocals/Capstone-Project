4941baf01c28b38f4f66dbcf0d0da1055134fce1
Abhi Handa <ahanda@ebay.com>
2019-12-27 22:33:56 -0800

    Blurrying the appbar image.

--- app/src/main/java/com/focals/myreddit/activity/PopularsActivity.java
+++ app/src/main/java/com/focals/myreddit/activity/PopularsActivity.java
@@ -1,9 +1,13 @@
 package com.focals.myreddit.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.focals.myreddit.R;
 import com.focals.myreddit.adapter.PopularsAdapter;
@@ -17,12 +21,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import jp.wasabeef.blurry.Blurry;
 
 public class PopularsActivity extends AppCompatActivity implements PopularsAdapter.ClickHandler {
 
     RecyclerView mainRecyclerView;
     ArrayList<Subreddit> subredditList;
     Toolbar toolbar;
+    ImageView iv_appbarImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,14 @@ public class PopularsActivity extends AppCompatActivity implements PopularsAdapt
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
+
+        iv_appbarImage = (ImageView) findViewById(R.id.expandedImage);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background1);
+
+        Blurry.with(this).radius(10).sampling(2).async().from(bitmap).into(iv_appbarImage);
+
+
     }
 
 
