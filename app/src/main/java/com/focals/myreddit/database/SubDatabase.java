441a936233726d6b3eac8891cd9c10ef79aea97f
Abhi Handa <ahanda@ebay.com>
2020-01-07 10:39:19 -0800

    Adding database class.

---
+++ app/src/main/java/com/focals/myreddit/database/SubDatabase.java
@@ -0,0 +1,26 @@
+package com.focals.myreddit.database;
+
+import android.content.Context;
+
+import com.focals.myreddit.data.Subreddit;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {SubEntity.class}, version = 1, exportSchema = false)
+public abstract class SubDatabase extends RoomDatabase {
+
+        private static final Object LOCK = new Object();
+        private static final String DATABASE_NAME = "subs";
+        private static SubDatabase instance;
+
+
+        public static SubDatabase getInstance(Context context) {
+            if (instance == null) {
+                synchronized (LOCK) {}
+                instance = Room.databaseBuilder(context.getApplicationContext(), SubDatabase.class, DATABASE_NAME).build();
+            }
+            return instance;
+        }
+}
