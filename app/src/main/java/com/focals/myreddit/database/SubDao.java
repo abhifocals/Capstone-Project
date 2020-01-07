3c90f7e9786d896d4d3d6ec626ab677bb0201d68
Abhi Handa <ahanda@ebay.com>
2020-01-07 15:39:46 -0800

    Added queries to dao.

--- app/src/main/java/com/focals/myreddit/database/SubDao.java
+++ app/src/main/java/com/focals/myreddit/database/SubDao.java
@@ -8,6 +8,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import static android.icu.text.MessagePattern.ArgType.SELECT;
 
@@ -17,7 +18,15 @@ public interface SubDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public void insertSubs(List<Subreddit> subs);
 
-    @Query("SELECT * FROM sub where id = :id")
-    public Subreddit getSubById(String id);
+    @Query("SELECT * FROM sub WHERE id = :providedId")
+    public Subreddit getSubById(String providedId);
 
+    @Query("SELECT favorite FROM sub WHERE id =:providedId")
+    public boolean isFavorite(String providedId);
+
+    @Query("UPDATE sub SET favorite = :isFavorite WHERE id = :providedId")
+    public void updateFavorite(String providedId, boolean isFavorite);
+
+    @Query("SELECT * FROM sub WHERE favorite")
+    public List<Subreddit> getFavorites();
 }
