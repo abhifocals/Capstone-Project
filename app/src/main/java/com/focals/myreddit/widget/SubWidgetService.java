87602e96eb8685279424eeb26f4e800f6d6e5667
Abhi Handa <ahanda@ebay.com>
2020-01-13 21:42:56 -0800

    Adding a WidgetService class for RecyclerView.

--- app/src/main/java/com/focals/myreddit/widget/SubWidgetService.java
+++ app/src/main/java/com/focals/myreddit/widget/SubWidgetService.java
@@ -1,17 +1,26 @@
 package com.focals.myreddit.widget;
 
+import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 public class SubWidgetService extends RemoteViewsService {
+
+
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new SubWidgetFactory(this);
     }
 }
 
 class SubWidgetFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    Context context;
+
+    public SubWidgetFactory(Context context) {
+        this.context = context;
+    }
 
     @Override
     public void onCreate() {
@@ -30,11 +39,19 @@ class SubWidgetFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public int getCount() {
-        return 0;
+        return 1;
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
+
+        //TODO
+
+
+
+
+
+
         return null;
     }
 
