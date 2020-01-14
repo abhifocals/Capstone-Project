4ee699e497210f6ad3a995ed4fc5d9bf0afb2ecf
Abhi Handa <ahanda@ebay.com>
2020-01-14 15:17:01 -0800

    Launching app when widget is clicked.

--- app/src/main/java/com/focals/myreddit/widget/SubWidgetProvider.java
+++ app/src/main/java/com/focals/myreddit/widget/SubWidgetProvider.java
@@ -1,5 +1,6 @@
 package com.focals.myreddit.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -7,8 +8,7 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.focals.myreddit.R;
-import com.focals.myreddit.database.SubDao;
-import com.focals.myreddit.database.SubDatabase;
+import com.focals.myreddit.activity.PopularsActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -21,6 +21,10 @@ public class SubWidgetProvider extends AppWidgetProvider {
         RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.sub_widget);
         Intent intent = new Intent(context, SubWidgetService.class);
         rv.setRemoteAdapter(R.id.lv_widget, intent);
+
+        Intent launchIntent = new Intent(context, PopularsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+        rv.setOnClickPendingIntent(R.id.tv_widgetTitle, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, rv);
