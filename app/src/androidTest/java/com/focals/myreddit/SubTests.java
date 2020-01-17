a24e6f3a1d720a8917c4a4523f3b07d98cbde92b
Abhi Handa <ahanda@ebay.com>
2020-01-17 13:48:52 -0800

    Adding more tests.

--- app/src/androidTest/java/com/focals/myreddit/SubTests.java
+++ app/src/androidTest/java/com/focals/myreddit/SubTests.java
@@ -8,14 +8,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -46,5 +50,45 @@ public class SubTests {
         onView(withId(R.id.rv_populars)).check(matches(TestHelper.atPosition(0,
                 hasDescendant(hasDescendant(withId(R.id.ib_addToFavorites))))));
 
+    }
+
+    @Test
+    public void testPostsActivitty() {
+
+        // Click on first Sub to get to Posts Activity
+        onView(withText("news")).perform(click());
+
+        // Verify same post is displayed in Posts Activity
+        // onView(allOf(withId(R.id.expandedImage), withText("news"))).check(matches(isDisplayed()));
+
+        // Verify add/remove fav button
+        onView(withId(R.id.ib_addToFavorites)).check(matches(isDisplayed()));
+
+        // Verify item has comments count
+        onView(withId(R.id.rv_posts)).check(matches(TestHelper.atPosition(0,
+                hasDescendant((withId(R.id.tv_commentsCount))))));
+    }
+
+    @Test
+    public void testCommentsActivitty() {
+
+        // Click on first Sub to get to Posts Activity
+        onView(withText("news")).perform(click());
+
+        // Click on first Post to get to Comments Activity
+        onView(withId(R.id.rv_posts)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Verify same post is displayed in Posts Activity
+        // onView(allOf(withId(R.id.expandedImage), withText("news"))).check(matches(isDisplayed()));
+
+        // Verify add/remove fav button
+        onView(withId(R.id.ib_addToFavorites)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.tv_postText)).check(matches(isDisplayed()));
+
+        // Verify item has comments count
+        onView(allOf(withId(R.id.rv_comments))).
+                check(matches(hasDescendant(hasDescendant(withId(R.id.tv_comment))))).
+                check(matches(isDisplayed()));
     }
 }
