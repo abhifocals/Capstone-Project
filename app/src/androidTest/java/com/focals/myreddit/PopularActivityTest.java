dea5124b1e383f912d435bfea23aa5282b97f62b
Abhi Handa <ahanda@ebay.com>
2020-01-17 11:22:21 -0800

    Adding a test.

--- app/src/androidTest/java/com/focals/myreddit/PopularActivityTest.java
+++ app/src/androidTest/java/com/focals/myreddit/PopularActivityTest.java
@@ -1,14 +1,31 @@
 package com.focals.myreddit;
 
 
+import android.util.EventLogTags;
+import android.view.View;
+
 import com.focals.myreddit.activity.PopularsActivity;
+import com.focals.myreddit.utils.RecyclerViewTestHelper;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 public class PopularActivityTest {
@@ -18,13 +35,23 @@ public class PopularActivityTest {
 
     @Test
     public void testPopularActivity() {
-        
-        System.out.println();
 
+        onView(withId(R.id.rv_populars)).check(matches(RecyclerViewTestHelper.atPosition(0,
+                ViewMatchers.hasDescendant(ViewMatchers.hasDescendant(withText("news"))))));
+
+        onView(withId(R.id.rv_populars)).check(matches(RecyclerViewTestHelper.atPosition(0,
+                ViewMatchers.hasDescendant(ViewMatchers.hasDescendant(withId(R.id.iv_subredditImage))))));
+
+        onView(withId(R.id.rv_populars)).check(matches(RecyclerViewTestHelper.atPosition(0,
+                ViewMatchers.hasDescendant(ViewMatchers.hasDescendant(withId(R.id.tv_subredditDesc))))));
+
+        onView(withId(R.id.rv_populars)).check(matches(RecyclerViewTestHelper.atPosition(0,
+                ViewMatchers.hasDescendant(ViewMatchers.hasDescendant(withId(R.id.tv_subscriberCount))))));
+
+        onView(withId(R.id.rv_populars)).check(matches(RecyclerViewTestHelper.atPosition(0,
+                ViewMatchers.hasDescendant(ViewMatchers.hasDescendant(withId(R.id.ib_addToFavorites))))));
 
     }
-
-
 
 
 }
