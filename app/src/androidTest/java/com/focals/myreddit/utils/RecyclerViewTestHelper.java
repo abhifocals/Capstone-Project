4889b605ffb5c63ed2bb931b2908d3357036c72d
Abhi Handa <ahanda@ebay.com>
2020-01-17 11:22:15 -0800

    Adding custom ViewMatcher.

---
+++ app/src/androidTest/java/com/focals/myreddit/utils/RecyclerViewTestHelper.java
@@ -0,0 +1,41 @@
+package com.focals.myreddit.utils;
+
+import android.view.View;
+import android.widget.EditText;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import static androidx.test.espresso.core.internal.deps.dagger.internal.Preconditions.checkNotNull;
+
+/**
+ * A custom matcher that checks the hint property of an {@link EditText}. It
+ * accepts either a {@link String} or a {@link Matcher}.
+ */
+public class RecyclerViewTestHelper {
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+}
