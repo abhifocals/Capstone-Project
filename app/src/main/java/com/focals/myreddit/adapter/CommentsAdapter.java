4e353c115ec66bd8bf7048dfed794ada26b37717
Abhi Handa <ahanda@ebay.com>
2020-01-26 00:28:37 -0800

    Working logic for displaying Comment Text.

--- app/src/main/java/com/focals/myreddit/adapter/CommentsAdapter.java
+++ app/src/main/java/com/focals/myreddit/adapter/CommentsAdapter.java
@@ -1,6 +1,7 @@
 package com.focals.myreddit.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,8 +9,14 @@ import android.widget.TextView;
 
 import com.focals.myreddit.R;
 import com.focals.myreddit.data.Comment;
+import com.focals.myreddit.data.RedditParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +25,9 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
 
     Context context;
     ArrayList<Comment> commentsList;
+    static ArrayList<Comment> childrenList;
+    static HashMap<String, ArrayList<Comment>> commentMap;
+    static String logTag = "Testing";
 
 
     public CommentsAdapter(ArrayList<Comment> commentsList) {
@@ -40,14 +50,126 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
     @Override
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
 
+
+        // Get 1st item from static listOfCommentsMap
+        commentMap = RedditParser.listOfCommentsMap.get(0);
+
+
+        // Get parent comment
+        String parentCommentText = commentMap.get("mainParent").get(0).getText();
+        String parentId = commentMap.get("mainParent").get(0).getId();
+        Log.d(logTag, parentCommentText);
+
+
+
+        // Get children of parent
+        ArrayList<Comment> childrenList = commentMap.get(parentId);
+
+        for (int i=0; i < childrenList.size(); i++) {
+            Log.d(logTag,  "  " + childrenList.get(i).getText());
+            getChildren(childrenList.get(i));
+        }
+
+
+
+
+
+
+        //  Print Text of each comment.  Add tab multiple of each comment.
+
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         holder.commentsTextView.setText(commentsList.get(position).getCommentBody());
 
     }
 
+    private static void getChildren(Comment comment) {
+        if (!childrenExist(comment)) {
+            return;
+
+        } else {
+            getAllChildren(comment);
+
+            for (int i = 0; i < commentMap.get(comment.getId()).size(); i++) {
+                getChildren(commentMap.get(comment.getId()).get(i));
+            }
+        }
+    }
+
+    private static void getAllChildren(Comment comment) {
+        ArrayList<Comment> children = commentMap.get(comment.getId());
+
+
+        for (int i = 0; i < children.size(); i++) {
+
+            Log.d(logTag,  "    " + children.get(i).getText());
+
+        }
+    }
+
+
+    private static boolean childrenExist(Comment comment) {
+        return commentMap.get(comment.getId()) != null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public int getItemCount() {
-        return commentsList.size();
+        return RedditParser.listOfCommentsMap.size();
     }
 
 
